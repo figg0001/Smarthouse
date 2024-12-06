3edb267117df807da407d5839a002ae250df0410
figg0001 <99642186+figg0001@users.noreply.github.com>
2024-12-05 20:11:40 -0500

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,14 @@
+#ifndef ARDUINO_SECRETS_H
+#define ARDUINO_SECRETS_H
+
+// WiFi credentials
+#define WIFI_SSID "Wifi name"
+#define WIFI_PASSWORD "wifi code"
+
+// Twilio Account Settings
+#define TWILIO_ACCOUNT_SID "enter"
+#define TWILIO_AUTH_TOKEN "enter"
+#define TWILIO_FROM_NUMBER "+enter"  // Your Twilio phone number
+#define TWILIO_TO_NUMBER "+enter"    // Recipient's phone number
+
+#endif
